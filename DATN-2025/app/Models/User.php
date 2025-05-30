fe1dfe03da9365c6c88a0f828af38c3af94e70f9
HoangVanLuong2207 <luonghvpp03220@gmail.com>
2025-05-30 16:31:27 +0700

    admin.staff

--- DATN-2025/app/Models/User.php
+++ DATN-2025/app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
+        'image',
+        'role',
+        'employee_id',
+        'status',
         'password',
     ];
 
--- DATN-2025/database/migrations/2014_10_12_000000_create_users_table.php
+++ DATN-2025/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,23 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable(); // Lưu đường dẫn file ảnh
             $table->string('password');
             $table->rememberToken();
-            $table->tinyInteger('role')->default(0);
-            $table->timestamps();
+
+            // Vai trò: Ví dụ 0=Customer, 1=Staff, 2=Admin
+            $table->tinyInteger('role')->default(0)->comment('0: Customer, 1: Admin, 21: Thu ngân, 22: Pha chế');
+
+            // Mã nhân viên (nếu có)
+            $table->string('employee_id')->nullable()->unique()->comment('Mã nhân viên (nếu có)');
+
+            // Trạng thái tài khoản
+            $table->tinyInteger('status')->default(1)->comment('0: Vô hiệu hóa, 1: Hoạt động, 2: Bị khóa');
+
+            $table->timestamps(); // created_at, updated_at
         });
+
     }
 
     /**
