61f3c4b06032404df4eb8cb2e207d3a2a81a9d28
HoangVanLuong2207 <luonghvpp03220@gmail.com>
2025-05-30 07:59:33 +0700

    update DBDB

--- DATN-2025/database/migrations/2025_05_12_011023_order.php
+++ DATN-2025/database/migrations/2025_05_12_011023_order.php
@@ -13,13 +13,13 @@ return new class extends Migration
 {
     Schema::create('order', function (Blueprint $table) {
         $table->id();
-        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-        $table->string('name', 255);         
-        $table->string('address', 255);      
-        $table->string('phone', 15);       
+        $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
+        $table->string('name', 255);
+        $table->string('address', 255);
+        $table->string('phone', 15);
         $table->enum('payment_method', ['cash', 'banking', 'momo'])->default('cash');
         $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending');
-        $table->decimal('total', 10, 2);     
+        $table->decimal('total', 10, 2);
         $table->timestamps();
     });
 }
