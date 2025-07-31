08758e8e7fbff421791f8b9d2b675462a9978b74
thong <thonghpc9825@gmail.com>
2025-07-31 17:16:56 +0700

    xoa mem blog

--- DATN-2025/app/Models/Blogs.php
+++ DATN-2025/app/Models/Blogs.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use function Laravel\Prompts\table;
 
 class Blogs extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected  $table = 'blogs';
     protected $fillable = ['title', 'content', 'image', 'blog_id'];
     public function danhmucBlog()
